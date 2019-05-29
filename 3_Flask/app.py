f2ebd08b0116e669622f1b111162b9ab226ec8dd
smnoh <findsolution88@gmail.com>
2019-05-29 14:34:30 +0900

    get_lotto, pick_lotto

--- 3_Flask/app.py
+++ 3_Flask/app.py
@@ -19,17 +19,22 @@ def lotto():
     # return f'이번주 로또 예상 번호는 {lucky_numbers}입니다.'
     return render_template('pick_lotto.html', numbers=lucky_numbers)
 
+
 @app.route('/find_lotto/<int:num>')
 def find_lotto(num):
     url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
-    data = requests.get(url).text
-    lotto_data = json.loads(data)
+    data = requests.get(url).text #type = string
+    lotto_data = json.loads(data) #type = dict
 
     lotto_num = []
-    for i in range(1,7):
-        lotto_num.append(lotto_data[f'drwtNo{i}'])
-    lotto_num.sort()
+    if lotto_data['returnValue'] == 'success':
+        for i in range(1,7):
+            lotto_num.append(lotto_data[f'drwtNo{i}'])
+        lotto_num.sort()
+
     return render_template('find_lotto.html', numbers=lotto_num, num=num)
+
+
 
 
 @app.route('/square/<int:num>')
