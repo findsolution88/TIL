2addcd5d0289aa381955b84ab3601a0986d6aea6
smnoh <findsolution88@gmail.com>
2019-06-05 13:48:47 +0900

    django | CRUD | 190605

--- 4_django/MODEL_BASIC/board/urls.py
+++ 4_django/MODEL_BASIC/board/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    # Create
+    path('new/', views.new_article), #/board/new/
+    path('create/', views.create_article), #/board/create
+
+    # Read
+    path('', views.article_list), #/board/
+    path('<int:article_id>/', views.article_detail), #/board/2
+
+    # Update
+
+    # Delete
+    path('<int:article_id>/delete/', views.delete_article), # /board/1/delete
 ]
--- 4_django/MODEL_BASIC/board/views.py
+++ 4_django/MODEL_BASIC/board/views.py
@@ -1,4 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
-def index(request):
-    return render(request, 'board/index.html')
+# Form
+def new_article(request):
+    return render(request, 'board/new_article.html')
+
+# SAVE article
+def create_article(request):
+    article = Article()
+
+    article.title = request.GET.get('title')
+    article.content = request.GET.get('content')
+
+    article.save()
+    return redirect(f'/board/{article.id}')
+
+# read all
+def article_list(request): # 전체 article 조회하기
+    articles = Article.objects.all()
+    return render(request, 'board/article_list.html', {
+        'articles': articles,
+    })
+
+# read one
+def article_detail(request, article_id): # 특정 article 조회하기
+    article = Article.objects.get(id=article_id)
+    return render(request, 'board/article_detail.html',{
+        'article': article,
+    })
+
+
+# todo: Update
+
+
+# Delete
+def delete_article(request, article_id):
+    article = Article.objects.get(id=article_id)
+    article.delete()
+    return redirect('/board/')
